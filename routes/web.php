a6a62b35b3c7abb85878642b276b9da64724bca6
AlbaCruz-hub <gabis200314@gmail.com>
2026-03-18 22:08:09 -0600

    Se agregaron métodos para la base de datos

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PrincipalController;
+use App\Models\Pagina;
 
 /*Route::get('/', function () {
     return view('welcome');
@@ -23,3 +24,107 @@ Route::get('/contact', function () {
     return view('contact', ['nombre' => $nombre,'carrera' => 'Licenciatura en Administración de TIC']);
 })->name('contact');
 
+//Insertar nuevos registros a la tabla
+
+Route::get('nuevoregistro', function(){
+
+    $pagina = new Pagina;
+    $pagina->name = 'CARLOS';
+    $pagina->email = 'maria@gmail.com';
+    $pagina->email_verified_at = date('Y-m-d');
+    $pagina->password = '123456';
+    $pagina->avatar = 'user.png';
+    $pagina->telefono = '999999';
+    $pagina->calle = 89;
+    $pagina->is_active = 1;
+    $pagina->save();
+
+    return $pagina;
+});
+
+// Definimos el método para buscar por el id
+// Para obtener unicamente un registro
+Route::get('buscarpaginaid', function(){
+
+    $post = Pagina::find(3);
+    return $post;
+
+});
+
+// Definimos el método para buscar por un campo determinado
+Route::get('buscarxname', function(){
+
+    $post = Pagina::where('name', 'carlos')->first();
+    return $post;
+
+});
+
+// Para recuperar más de un registro
+Route::get('obtenerTodos', function(){
+
+    $post = Pagina::all();
+    return $post;
+
+});
+
+// Definimos el método para cambiar un registro
+Route::get('updatename', function(){
+
+    $post = Pagina::where('name', 'CARLOS')->first();
+    $post->email = 'carlos2026@gmail.com';
+    $post->save();
+
+    return $post;
+
+});
+
+// Definimos un método para obtener una lista conforme a un criterio determinado
+// Para obtener más de un registro
+Route::get('filter', function(){
+
+    //$post = Pagina::where('calle', 'like', '%123%')->get();
+    $post = Pagina::where('calle', 'like', '%123%')->orderBy('id', 'desc')->get();
+    return $post;
+
+});
+
+// Para especificar unicamente los campos que quiera
+Route::get('trescampos', function(){
+
+    $post = Pagina::select('name', 'email', 'telefono')->get();
+    return $post;
+
+});
+
+// Conforme a una selección solamente traerme un cierto número de registros
+Route::get('filtroxnumreg', function(){
+
+    $post = Pagina::select('name', 'email')->orderBy('name')->take(2)->get();
+    return $post;
+
+});
+
+// Para eliminar un determinado registro
+Route::get('eliminar_registro', function(){
+
+    $post = Pagina::find(9);
+    $post->delete();
+    return "Eliminado";
+
+});
+
+// Obtener la fecha conforme a un formato
+Route::get('obtenerfechaformato', function(){
+
+    $post = Pagina::select('name', 'email', 'created_at')->find(3);
+    return $post;
+
+});
+
+// Obtener el valor de is_active
+Route::get('obtenerestatus', function(){
+
+    $post = Pagina::find(1);
+    dd($post->is_active);
+
+});
